2272226664af44a3ca7202414a3947c9454b705b
Mashihur <sarker.afm@gmail.com>
2025-10-22 14:14:16 +0600

    code

---
+++ Desktop/Compiler.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    5=2;
+    return 0;
+}
---
+++ Desktop/ROHAN/hi.py
@@ -0,0 +1,10 @@
+temp = float(input("Enter the current temparature: ") )
+print (temp)
+
+if temp>25:
+    print("Wear Light cloth.")
+elif 15<= temp <=25:
+    print("Wear Normal Cloth.")
+else:
+    print("Wear arm cloth.")
+
